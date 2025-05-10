1449b0b31df273ae4cc34692ec32dfd61ce88cc4
unknown <prantokih42@gmail.com>
2025-05-10 23:33:52 +0600

    first commit

---
+++ src/testone.ts
@@ -0,0 +1,6 @@
+const str : string = "hello";
+console.log(str);
+// ss bb u nn ===> string ,symbol ,bigint ,boolean , undefined , number , null  are primitive data types
+// array, obj , function are non primitive data types
+
+let  anyVar = 1 ; // if we use const we wont able to reassign the value
