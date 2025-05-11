4926c801b5c5eec0bdaf75d4ef8b6ce2edbcf8cf
unknown <prantokih42@gmail.com>
2025-05-11 23:50:30 +0600

    type allias added

--- src/testone.ts
+++ src/testone.ts
@@ -3,4 +3,25 @@ console.log(str);
 // ss bb u nn ===> string ,symbol ,bigint ,boolean , undefined , number , null  are primitive data types
 // array, obj , function are non primitive data types
 
-let  anyVar = 1 ; // if we use const we wont able to reassign the value
+let  anyVar : number = 1 ; // if we use const we wont able to reassign the value
+
+let arr :(number | string)[] = [1,2,3,4,5,'str']
+
+
+//type alias 
+type TObj = {
+    name : string,
+    age : number,
+    education : {
+        degree : string,
+        college : string
+    }
+}
+const obj : TObj = {
+    name : 'pranto',
+    age : 20,
+    education:{
+        degree : 'btech',
+        college : 'buet'
+    }
+}
